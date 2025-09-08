adade7f26ae40b6df337abb1cfde48fc8ee583f3
Unai <184846045+Gutifx@users.noreply.github.com>
2025-09-08 16:36:26 +0200

    10-leapYears completed

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return year % 4? false : (year % 100 ? true : (year % 400? false : true))
 };
 
 // Do not edit below this line
