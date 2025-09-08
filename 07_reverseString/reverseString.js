007b60abbb9ca3080dd42734c954414e8483898c
Unai <184846045+Gutifx@users.noreply.github.com>
2025-09-08 15:34:46 +0200

    07-reverseString completed

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    ans ="";
+    for (let i = str.length; i>0; i--) ans = ans + str[i - 1]
+    return ans
 };
 
 // Do not edit below this line
