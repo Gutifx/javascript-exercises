6826636875ef2b4988896961385c0e4b42dbd25c
Unai <184846045+Gutifx@users.noreply.github.com>
2025-09-08 16:03:05 +0200

    08-removeFromArray completed

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    for (const arg of args) {
+        let ind
+        while ((ind = arr.findIndex(x => x === arg)) != -1) arr.splice(ind, 1)
+    }
+    return arr
+
 };
 
 // Do not edit below this line
