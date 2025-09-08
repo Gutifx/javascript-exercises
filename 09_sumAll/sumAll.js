c3824779942b6d2ad6d01cd670b0aba80ffeb5fb
Unai <184846045+Gutifx@users.noreply.github.com>
2025-09-08 16:30:21 +0200

    09-sumAll completed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) return "ERROR"
+    ans = 0;
+    for (let i = Math.min(a, b); i<= Math.max(a, b); i++) ans += i;
+    return ans;
 };
 
 // Do not edit below this line
