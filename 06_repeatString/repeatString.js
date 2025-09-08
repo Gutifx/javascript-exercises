328c4e401334bd20e8bdcf6971b9e4dc7d131e12
Unai <184846045+Gutifx@users.noreply.github.com>
2025-09-08 15:30:06 +0200

    06-repeatString completed

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num<0) return "ERROR"
+    ans = ""
+    for(let i = 0; i < num; i++) ans = ans + str
+    return ans
 };
 
 // Do not edit below this line
