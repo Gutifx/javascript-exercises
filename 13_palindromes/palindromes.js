51da786ccaad68b552c76a116a1381cae4485808
Unai <184846045+Gutifx@users.noreply.github.com>
2025-09-12 22:43:58 +0200

    13_palidromes completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replaceAll(",", "").replaceAll("!", "").replaceAll(".", "").replaceAll(" ", "").toUpperCase()
+    console.log(str)
+    rts = [...str].reduce((acc, char) => char + acc, "")
+    return str == rts
 };
 
 // Do not edit below this line
