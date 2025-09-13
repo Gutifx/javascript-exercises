42318f93bcf235f5c7f71c727b91d6dc3dc31a8a
Unai <184846045+Gutifx@users.noreply.github.com>
2025-09-13 15:56:52 +0200

    14_fibonacci completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n
+    if (n < 0) return "OOPS"
+    if(!n) return 0
+    ans = 1
+    prev = 0
+    for (let i=1; i<n; i++) {
+        [ans, prev] = [ans + prev, ans]
+    }
+    return ans
 };
 
 // Do not edit below this line
